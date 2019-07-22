674a5f422d05995229f324bdb5722d98663f63c4
youyou <1094465220@qq.com>
2019-07-22 19:33:53 +0800

    zuoye

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,47 @@
 -- **数据库级别：**  
---  显示所有数据库  
+--  显示所有数据库   
+show databases;
 
 --  进入某个数据库  
+use student_examination_sys;
 
 --  创建一个数据库  
+create database ceshi2;
 
 --  创建指定字符集的数据库  
 
+create database test2 DEFAULT CHARACTER SET gbk COLLATE gbk_chinese_ci;
 --  显示数据库的创建信息   
-
+status;
 --  修改数据库的编码  
+alter database enterprises character set utf8;
 
 --  删除一个数据库   
+drop database ceshi2;
 
 -- **表级别**
 --  修改表名
-
+alter tableuser10 rename to user11;
 --  修改字段的数据类型
+alter table user11 modify column id int(20) ;
 
 --  修改字段名
+alter table user11 change id id1 int(20) not null,
 
 --  添加字段
+-- 增加一个字段，默认为空
+alter table user11 add COLUMN new1 VARCHAR(20) DEFAULT NULL; 
+-- 增加一个字段，默认不能为空
+alter table user11 add COLUMN new2 VARCHAR(20) NOT NULL;
 
 --  删除字段
+alter table user11 DROP COLUMN new2;
 
 --  修改表的存储引擎
 
+alter table user11 type=InnoDB;
 --  删除表的外键约束
 
+alter table vip drop foreign key FK1C81D1738DA76;
 --  删除一张表
+drop table user11 ;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,32 @@
 -- 插入记录  
+--语法
+INSERT INTO table_name ( field1, field2,...fieldN )
+                       VALUES
+                       ( value1, value2,...valueN );
+-- 实例
+INSERT INTO runoob_tbl (runoob_title, runoob_author, submission_date)
+VALUES ("学习 PHP", "菜鸟教程", NOW());
+
 
 -- 修改记录  
+--语法
+update 表名 set 列名1=expr1,列名2=expr2,......where 条件；
+--实例
+update user11 set id=19;
 
---  删除记录  
+
+-- 删除记录  
+--语法
+DELETE FROM table_name  WHERE 条件;
+--实例
+DELETE FROM user11 WHERE id=19;
+
 
 -- 查询记录  
+--语法
+SELECT column_name,column_name 
+FROM table_name
+[WHERE Clause]
+[LIMIT N][ OFFSET M]
+--实例
+select id,name,age from students;
